022fb9150a0598d095bab535c6fbf5fd7d075225
taylordigital13 <48325818+taylordigital13@users.noreply.github.com>
2019-03-07 10:45:21 -0500

    Add files via upload
    Initial commit

---
+++ VuforiaTools/Editor/JsonPost.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JsonPost
+{
+    public string name;
+    public float width;
+    public string image;
+    public bool active_flag;
+    public string application_metadata;
+
+    public JsonPost(string nameString, float widthNum, string imageString, string metaString)
+    {
+        name = nameString;
+        width = widthNum;
+        image = imageString;
+        active_flag = true;
+        application_metadata = metaString;
+    }
+
+    public JsonPost(string nameString, float widthNum, string imageString, bool activeBool, string metaString)
+    {
+        name = nameString;
+        width = widthNum;
+        image = imageString;
+        active_flag = activeBool;
+        application_metadata = metaString;
+    }
+}
